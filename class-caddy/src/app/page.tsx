4c8aa7847e42c12fa1e0f782f4d94e72bbf90eed
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-10-27 16:53:56 -0400

    Added delete event functionality

--- class-caddy/src/app/page.tsx
+++ class-caddy/src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 import Link from "next/link";
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin, {Draggable, DropArg} from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import {DialogPanel, Transition, TransitionChild} from '@headlessui/react'
+import {Dialog} from '@headlessui/react'
 
 import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 interface Event{
 title: string;
@@ -40,10 +43,10 @@ export default async function Home() {
     setNewEvent({...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime()})
     setShowModal(true)}
 
-    function addEvent(data:DropArg){
-      const event= (...newEvent, start: data.date.toISOString(), title: data.draggedEl.innerText, allDay: data.allDay, id: new Date().getTime())
-      setAllEvents([...allEvents, event])
-    }
+  function handleDeleteModal(data: {event: {id:string}}){
+    setShowDeleteModal(true);
+    setIdToDelete(Number(data.event.id));
+  }
    return (
     <HydrateClient>
       <nav className = "flex justify-between mb-12 border-b border-violet-100 p-4">
@@ -65,18 +68,54 @@ export default async function Home() {
                   right: 'resourceTimelineWork, dayGridMonth,timeGridWeek, timeGridDay'
 
                 }}
-                events={allEvents}
+                events={[]}
                 nowIndicator={true}
                 editable={true}
                 droppable={true}
                 selectable={true}
                 selectMirror={true}
                 dateClick={handleDateClick}
-                drop={(data)=> addEvent(data)}
                 eventClick={(data)=>handleDeleteModal(data)}
 
             />
 
+          </div>
+        </div>
+        <Transition show={showDeleteModal} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setShowDeleteModal}>
+                <TransitionChild 
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo='opacity-100'
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0">
+                </TransitionChild>
+          </Dialog>
+        </Transition>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <TransitionChild 
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true"/>
+                  </div>
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    
+                  </div>
+                </div>
+
+              </DialogPanel>
+            </TransitionChild>
           </div>
         </div>
       </main>
