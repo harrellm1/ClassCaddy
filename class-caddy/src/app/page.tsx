fa495d89d344ebbde2442c608d80a07c8ec853aa
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-10-29 16:55:59 -0400

    Changes to run dev

--- class-caddy/src/app/page.tsx
+++ class-caddy/src/app/page.tsx
@@ -9,8 +9,8 @@ import {DialogPanel, Transition, TransitionChild} from '@headlessui/react'
 import {Dialog} from '@headlessui/react'
 
 import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+//import { getServerAuthSession } from "~/server/auth";
+//import { api, HydrateClient } from "~/trpc/server";
 import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 interface Event{
@@ -20,14 +20,15 @@ allDay: boolean;
 id: number;
 }
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
+export default function Home() {
+  //const hello = await api.post.hello({ text: "from tRPC" });
+  //const session = await getServerAuthSession();
 
-  void api.post.getLatest.prefetch();
+  //void api.post.getLatest.prefetch();
   const[events, setEvents] = useState([
     {title: 'event 1', id: '1'}
   ])
+  
   const[allEvents, setAllEvents] = useState<Event[]>([])
   const [showmodal, setShowModal] = useState(false)
   const [showDeleteModal, setShowDeleteModal] = useState(false)
@@ -48,7 +49,7 @@ export default async function Home() {
     setIdToDelete(Number(data.event.id));
   }
    return (
-    <HydrateClient>
+    <div>
       <nav className = "flex justify-between mb-12 border-b border-violet-100 p-4">
         <h1 className="font-bold text-2x1 text-gray-700">
           Calendar
@@ -91,34 +92,35 @@ export default async function Home() {
                   leave="ease-in duration-200"
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0">
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                 </TransitionChild>
-          </Dialog>
-        </Transition>
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <TransitionChild 
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true"/>
-                  </div>
-                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    
-                  </div>
-                </div>
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <TransitionChild 
+                        as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                            <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                  <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true"/>
+                                </div>
+                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                  
+                                </div>
+                              </div>
 
               </DialogPanel>
             </TransitionChild>
           </div>
         </div>
+          </Dialog>
+        </Transition>
       </main>
-    </HydrateClient>
+    </div>
   );
 }
