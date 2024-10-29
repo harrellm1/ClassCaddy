a8ee7f92b01bb9af5515e933fd1bbee02f59feb8
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-10-29 17:34:25 -0400

    finished delete event modal

--- class-caddy/src/app/page.tsx
+++ class-caddy/src/app/page.tsx
@@ -5,7 +5,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin, {Draggable, DropArg} from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import {DialogPanel, Transition, TransitionChild} from '@headlessui/react'
+import {DialogPanel, DialogTitle, Transition, TransitionChild} from '@headlessui/react'
 import {Dialog} from '@headlessui/react'
 
 import { LatestPost } from "~/app/_components/post";
@@ -34,6 +34,7 @@ export default function Home() {
   const [showDeleteModal, setShowDeleteModal] = useState(false)
   const [idToDelete, setIdToDelete] = useState<number | null>(null)
   const[newEvent, setNewEvent] = useState<Event>({
+    //pass this information to database?
     title: '',
     start: '',
     allDay: false,
@@ -47,6 +48,23 @@ export default function Home() {
   function handleDeleteModal(data: {event: {id:string}}){
     setShowDeleteModal(true);
     setIdToDelete(Number(data.event.id));
+  }
+
+  function handleDelete(){
+    //filters through events and finds which one to delete
+    //also delete event from database
+    setAllEvents(allEvents.filter(event => Number(event.id) != Number(idToDelete)))
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+  function handleCloseModal(){
+    setShowModal(false)
+    setNewEvent({
+      title:"",
+      start:"",
+      allDay:false,
+      id:0
+    })
   }
    return (
     <div>
@@ -105,21 +123,39 @@ export default function Home() {
                           leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                           leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                             <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                              <div className="sm:flex sm:items-start">
                               <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                 <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                                   <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true"/>
                                 </div>
                                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                  
+                                  <DialogTitle as="h3" className="test-base font-semibold leading-6 text-grey-900">
+                                    Delete Event
+                                  </DialogTitle>
+                                  <div className="mt-2">
+                                    <p className="test-sm text-gray-500">
+                                      Delete this event?
+                                    </p>
+                                  </div>
                                 </div>
                               </div>
-
-              </DialogPanel>
-            </TransitionChild>
-          </div>
-        </div>
-          </Dialog>
-        </Transition>
+                              </div>
+                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                              <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text:sm 
+                              font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" onClick={handleDelete}>
+                                Delete
+                              </button>
+                              <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text:sm
+                               font-semibold shadow-sm ring-1 ring-insert ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={handleCloseModal}>
+                                Cancel
+                              </button>
+                            </div>
+                            </DialogPanel>
+                      </TransitionChild>
+                    </div>
+                  </div>
+            </Dialog>
+          </Transition>
       </main>
     </div>
   );
